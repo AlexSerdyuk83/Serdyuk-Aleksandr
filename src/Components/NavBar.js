2176b79ac8e77e9fbb2e421013608f6da4a6972f
aleksandrserdyuk <av.serdiuk@yandex.by>
2021-12-19 17:25:26 +0300

    initial commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import avatar from '../img/avatar.jpg';
+import HomePage from '../Pages/HomePage';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <nav className="nav">
+        <div className="profile">
+          <img src={avatar} alt=""/>
+        </div>
+        <ul className="nav-items">
+          <li className="nav-item">
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" exact activeClassName="active">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/portfolio" exact activeClassName="active">
+              Portfolio
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/blogs" exact activeClassName="active">
+              Blogs
+            </NavLink>
+            <li className="nav-item">
+              <NavLink to="/contacts" exact activeClassName="active">
+                Contacts
+              </NavLink>
+            </li>
+          </li>
+        </ul>
+        <footer className="footer">
+          <p>
+            @2021 Lorem Ipsum
+          </p>
+        </footer>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="hero">
+        <h1 className="hero-text">
+          Hi, i am <span>Aleksandr Serdyuk</span>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
