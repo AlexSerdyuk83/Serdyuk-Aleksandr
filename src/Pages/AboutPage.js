13a9115b6c13df0b8f7364829a02a3a39a5c2562
aleksandrserdyuk <av.serdiuk@yandex.by>
2022-01-08 16:38:11 +0300

    add services section

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Title from '../Components/Title';
 import ImageSection from './ImageSection';
 import SkillsSection from '../Components/SkillsSection';
+import ServicesSection from '../Components/ServicesSection';
 import {skills} from '../utilits/skills';
+import {services} from '../utilits/services';
 
 function AboutPage() {
   return (
@@ -13,6 +15,18 @@ function AboutPage() {
       <div className="skillsContainer">
         {
           skills.map(({name, progress, width}) => <SkillsSection skill={name} progress={progress} width={width}/>)
+        }
+      </div>
+      <Title title={'Services'} span={'Services'}/>
+      <div className="ServicesContainer">
+        {
+          services.map(({name, image, text}) =>
+            <ServicesSection
+              image={image}
+              title={name}
+              text={text}
+            />
+          )
         }
       </div>
     </div>
---
+++ src/utilits/services.js
@@ -0,0 +1,9 @@
+import design from '../img/design.svg';
+import intelligence from '../img/intelligence.svg';
+import gameDev from '../img/game-dev.svg';
+
+export const services = [
+  {name: 'Web design', image: design, text: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.'},
+  {name: 'Frontend development', image: intelligence, text: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.'},
+  {name: 'Backend development', image: gameDev, text: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit.'},
+]
